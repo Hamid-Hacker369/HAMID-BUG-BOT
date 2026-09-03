b3731ab6c767b3e6fda1c728a95e61c651d06641
Hamid-Hacker369 <hamid-hacker369@users.noreply.github.com>
2026-09-03 14:41:22 +0000

    Initial upload

---
+++ config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    mainToken: process.env.BOT_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN', // Bot Token Here 
+    
+    S7: '☠︎〆 𝙃𝘼𝙈𝙄𝘿 𝘽𝙐𝙂 𝘽𝙊𝙏 〆☠︎', // Owner, Name
+    
+    adminId: process.env.ADMIN_ID || 'YOUR_ADMIN_CHAT_ID', // Owner Chat ID ( control /address, /delresell )
+    
+    channel: 'https://t.me/hk_313_team', // TG channel link
+    
+    group: 'https://t.me/hk_313_team1', // TG group link
+    
+    bot: '☠︎〆 𝙃𝘼𝙈𝙄𝘿 𝘽𝙐𝙂 𝘽𝙊𝙏 〆☠', // Bot name 
+    
+    logo: './SY/Loves.jpg' // Bot logo path or URL
+};
