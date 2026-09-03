5bed147ee97352679e93c52c835d80b47834bd61
Hamid-Hacker369 <ahmadabdulhameed5490@gmail.com>
2026-09-04 02:26:55 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-    mainToken: process.env.BOT_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN', // Bot Token Here 
+    mainToken: process.env.BOT_TOKEN || '8910494264:AAEthbkao2W3dnpsOCMJuOrTG8TQBltEPKg', // Bot Token Here 
     
     S7: '☠︎〆 𝙃𝘼𝙈𝙄𝘿 𝘽𝙐𝙂 𝘽𝙊𝙏 〆☠︎', // Owner, Name
     
-    adminId: process.env.ADMIN_ID || 'YOUR_ADMIN_CHAT_ID', // Owner Chat ID ( control /address, /delresell )
+    adminId: process.env.ADMIN_ID || '6574042583', // Owner Chat ID ( control /address, /delresell )
     
     channel: 'https://t.me/hk_313_team', // TG channel link
     
